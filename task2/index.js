edef9d40a9f57867e11bdb909f0480440bb1284f
Shved Anastasiia <shvednastya0@gmail.com>
2022-04-01 02:12:15 +0300

    Add task 2

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,26 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(n) {
+
+  let displace = 0;
+
+  for (let i = 0; i < n; i++) {
+
+      let row = "";
+
+      for (let k = 97 + displace; k < 97 + n + displace; k++) {
+
+          let letter = k;
+
+          if (letter > 122) {
+
+              letter -= 26 * Math.floor((letter - 97) / 26);
+          }
+
+          row = row + String.fromCharCode(letter) + " ";
+      }
+
+      displace++;
+      console.log(row, "\n");
+  }
+
 }
+calculate(50)
